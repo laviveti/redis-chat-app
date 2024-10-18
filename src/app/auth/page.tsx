8ef5672e27b3df549908078503d2094908b6b631
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-10-18 10:45:36 -0300

    feat: adicionando componentes shadcn: avatar dialog popove r resizable scroll-area skeleton textarea tooltip

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const page = () => {
-  return <div>Auth page</div>;
+  return (
+    <div>
+      Auth page
+      <Button>Button</Button>
+    </div>
+  );
 };
 
 export default page;
