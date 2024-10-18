193ac4aa65db7eb79b55acd655b97a6f0322aa09
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-10-18 16:51:17 -0300

    feat(45:47): personalizando eslint

--- src/components/preferences-tab.tsx
+++ src/components/preferences-tab.tsx
@@ -1,18 +1,51 @@
+"use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { MoonIcon, SunIcon, Volume2 } from "lucide-react";
+import { MoonIcon, SunIcon, Volume2, VolumeX } from "lucide-react";
+import { useTheme } from "next-themes";
+import { usePreferences } from "../../store/use-preferences";
+import { useSound } from "use-sound";
 
 export const PreferencesTab = () => {
+  const { setTheme } = useTheme();
+
+  const { soundEnabled, setSoundEnabled } = usePreferences();
+  const [playMouseClick] = useSound("/sounds/mouse-click.mp3");
+  const [playSoundOn] = useSound("/sounds/sound-on.mp3", { volume: 0.3 });
+  const [playSoundOff] = useSound("/sounds/sound-off.mp3", { volume: 0.3 });
+
   return (
     <div className='flex flex-wrap gap-2 px-1 md:px-2'>
-      <Button variant={"outline"} size={"icon"}>
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        onClick={() => {
+          setTheme("light");
+          if (soundEnabled) playMouseClick();
+        }}>
         <SunIcon className='size-[1.2rem] text-muted-foreground' />
       </Button>
-      <Button variant={"outline"} size={"icon"}>
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        onClick={() => {
+          setTheme("dark");
+          if (soundEnabled) playMouseClick();
+        }}>
         <MoonIcon className='size-[1.2rem] text-muted-foreground' />
       </Button>
-      <Button variant={"outline"} size={"icon"}>
-        <Volume2 className='size-[1.2rem] text-muted-foreground' />
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        onClick={() => {
+          setSoundEnabled(!soundEnabled);
+          soundEnabled ? playSoundOff() : playSoundOn();
+        }}>
+        {soundEnabled ? (
+          <Volume2 className='size-[1.2rem] text-muted-foreground' />
+        ) : (
+          <VolumeX className='size-[1.2rem] text-muted-foreground' />
+        )}
       </Button>
     </div>
   );
