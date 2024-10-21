445c303e4190ff3cf175fe5e29cefed2e8365075
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-10-21 17:10:13 -0300

    feat(2h06): estilizando scrollbar em globals.css

--- src/components/chat/chat-bottom-bar.tsx
+++ src/components/chat/chat-bottom-bar.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { ImageIcon } from "lucide-react";
+import { ImageIcon, Loader, SendHorizonal, ThumbsUp } from "lucide-react";
 import { Textarea } from "@/components/ui/textarea";
 import { EmojiPicker } from "./emoji-picker";
+import { Button } from "../ui/button";
 
 export const ChatBottomBar = () => {
   const [message, setMessage] = React.useState("");
+  const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+  const isPending = false;
+
   return (
     <div className='p-2 flex justify-between w-full items-center gap-2'>
       {!message.trim() && <ImageIcon size={20} className='cursor-pointer text-muted-foreground' />}
@@ -27,11 +31,33 @@ export const ChatBottomBar = () => {
             className='w-full border rounded-full flex items-center h-9 resize-none overflow-hidden bg-background min-h-0'
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            ref={textAreaRef}
           />
           <div className='absolute right-2 bottom-0.5'>
-            <EmojiPicker onChange={(emoji) => setMessage(message + emoji)} />
+            <EmojiPicker
+              onChange={(emoji) => {
+                setMessage(message + emoji);
+                if (textAreaRef.current) textAreaRef.current.focus();
+              }}
+            />
           </div>
         </motion.div>
+        {message.trim() ? (
+          <Button
+            className='size-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0'
+            variant={"ghost"}
+            size={"icon"}>
+            <SendHorizonal size={20} className='text-muted-foreground' />
+          </Button>
+        ) : (
+          <Button
+            className='size-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0'
+            variant={"ghost"}
+            size={"icon"}>
+            {!isPending && <ThumbsUp size={20} className='text-muted-foreground' />}
+            {isPending && <Loader size={20} className='animate-spin' />}
+          </Button>
+        )}
       </AnimatePresence>
     </div>
   );
