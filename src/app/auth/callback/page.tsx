ee36d9594d66d7d3c50578aa75f7d490b2f8b481
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-10-22 11:08:58 -0300

    feat(2h54): implementando autenticação com Kinde e Upstash

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,11 +1,29 @@
+"use client";
 import React from "react";
+import { Loader } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { checkAuthStatus } from "@/actions/auth.actions";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const Page = () => {
+  const router = useRouter();
+  const { data } = useQuery({
+    queryKey: ["authCheck"],
+    queryFn: async () => await checkAuthStatus(),
+  });
+
+  if (data?.success) router.push("/");
+
   return (
     <div className='mt-20 w-full flex justify-center'>
-      <div className='flex flex-col items-center gap-2'></div>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader className='size-10 animate-spin text-muted-foreground' />
+        <h3 className='text-xl font-bold'>Redirecting...</h3>
+
+        <p>Please wait</p>
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
